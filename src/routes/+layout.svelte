a6ad9182281a5f2757d149f56861a994c951258c
nontawit <ntwitmn@gmail.com>
2025-11-14 10:10:34 +0700

    all new

--- src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-    // Import CSS เพื่อให้ Tailwind CSS ทำงานได้
-    import '../app.css'; 
-</script>
-
-<div class="min-h-screen bg-gray-100 antialiased">
-    <slot />
-</div>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
-});
+  plugins: [react()],
+})
